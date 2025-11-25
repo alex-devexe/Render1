d102be15dc784273cb08ae9b675292d9aec0d1ad
Myers <cevallosantanaa@gmail.com>
2025-11-25 10:36:56 -0500

    Agregar .gitignore y remover .env del repositorio 1

--- db.js
+++ db.js
@@ -1,14 +1,19 @@
 const mysql = require("mysql2");
+require("dotenv").config();  // cargar variables del .env
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "node_login"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
-    if (err) throw err;
+    if (err) {
+        console.error("Error al conectar a la BD:", err);
+        return;
+    }
     console.log("Base de datos conectada.");
 });
 
